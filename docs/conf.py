5d357f5be5bd50fe9ddf8fab2b7464b6472208eb
LaXispa33 <brice.leal@outlook.fr>
2021-10-03 17:53:38 +0200

    Doc generation fix

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../scripts/'))
+sys.path.insert(0, os.path.abspath('../scripts/ceps/binance/'))
+sys.path.insert(0, os.path.abspath('../scripts/ceps/bybit/'))
 
 
 # -- Project information -----------------------------------------------------
