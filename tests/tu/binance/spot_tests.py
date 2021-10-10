8ccc733fcf1f0af9ecbd583713c382b1d10699b5
LaXispa33 <brice.leal@outlook.fr>
2021-10-10 23:08:51 +0200

    Modifying pipeline

--- tests/tu/binance/spot_tests.py
+++ tests/tu/binance/spot_tests.py
@@ -21,7 +21,6 @@ import pytest
 class SequentialTestLoader(unittest.TestLoader):
     def getTestCaseNames(self, testCaseClass):
         test_names = super().getTestCaseNames(testCaseClass)
-        print(test_names)
         testcase_methods = list(testCaseClass.__dict__.keys())
         test_names.sort(key=testcase_methods.index)
         return test_names
